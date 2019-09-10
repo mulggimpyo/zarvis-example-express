a771d860bfed70b8855e8592734db219b4b4bb76
OJH <ceo@mulggimpyo.com>
2019-09-10 10:40:08 +0900

    wow

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Z.A.R.V.I.S. example' });
+
+  while(true){
+    res.render('index',{ title: 'ShutDown!!'});
+  }
+
 });
 
 module.exports = router;
